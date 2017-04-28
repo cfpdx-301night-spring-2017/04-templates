ce6fd2b3b4c10ca945e2884a39951c969b21e50e
erdem koral <koral.erdem@gmail.com>
2017-04-27 20:58:10 -0700

    completed TODO requiremnts

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -14,7 +14,8 @@ function Article (opts) {
 Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM and "compile" your template with Handlebars.
-
+  var template = $('#blogTemplate').html();
+  var compile = Handlebars.compile(template);
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -23,7 +24,8 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  var newDom = compile(this);
+  $('main').append(newDom);
 };
 
 rawData.sort(function(a,b) {
